3a12b7fbd1dd1af1789c8967a235a85a01a3dc49
asbilim <bilimdepaullilian@gmail.com>
2024-07-28 10:05:26 +0100

    stay up added

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,10 +1,32 @@
 import ShopMain from "@/components/layout/main";
 import { revalidateTag } from "next/cache";
 import { getProducts } from "@/server/get-products";
-import { notFound } from "next/navigation";
+
 export default async function Home() {
   revalidateTag("products");
   const products = await getProducts();
+  const url = `https://store-r1vy.onrender.com/`;
+  const interval = 30000;
+
+  function reloadWebsite() {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(
+          `Reloaded at ${new Date().toISOString()}: Status Code ${
+            response.status
+          }`
+        );
+      })
+      .catch((error) => {
+        console.error(
+          `Error reloading at ${new Date().toISOString()}:`,
+          error.message
+        );
+      });
+  }
+
+  setInterval(reloadWebsite, interval);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-24 px-4 md:px-8 lg:px-12">
