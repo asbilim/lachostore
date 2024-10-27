10880876d5946fee67d99d64d65f07f4ddfe3306
asbilim <bilimdepaullilian@gmail.com>
2024-10-27 23:45:02 +0100

    no more showing the default lachofit shop in home

--- components/layout/main.jsx
+++ components/layout/main.jsx
@@ -55,9 +55,11 @@ export default function ShopMain({
       <div className="w-full flex flex-col max-w-6xl  gap-4">
         <h2 className="text-primary mb-12">{t("section_two.subtitle")}</h2>
         <div className="grid w-full grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {recommended_products.map((item) => {
-            return <MainPageProductCard item={item} key={item.name} />;
-          })}
+          {Array.isArray(recommended_products)
+            ? recommended_products.map((item) => (
+                <MainPageProductCard item={item} key={item.name} />
+              ))
+            : null}
         </div>
       </div>
       <div className="w-full flex flex-col max-w-6xl gap-4 my-16">
